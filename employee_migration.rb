a5ab51c7824324490b2d3b809ba3e4e15a08424d
David Bernheisel <david@bernheisel.com>
2015-09-17 08:55:19 -0400

    Update employee_migration

--- employee_migration.rb
+++ employee_migration.rb
@@ -1,13 +1,21 @@
 require 'active_record'
 
-def EmployeeMigration < ActiveRecord::Migration
+ActiveRecord::Base.establish_connection(
+  adapter: 'sqlite3',
+  database: 'development.sqlite3' #name of file on disk
+)
+
+class EmployeeMigration < ActiveRecord::Migration
   def change
     create_table :employees do |t|
-      t.string, :name
-      t.string, :phone
-      t.boolean, :satisfactory
-      t.decimal, :salary, precision: 5, scale: 2
-      t.timestamps, null: false
+      t.string :name
+      t.string :phone
+      t.string :email
+      t.integer :department_id
+      t.boolean :satisfactory, default: true
+      t.integer :salary
+      #t.decimal :salary, precision: 5, scale: 2
+      t.timestamps null: false
     end
   end
 end
